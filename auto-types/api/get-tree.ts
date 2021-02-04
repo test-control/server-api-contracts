a2c6ffa55a6621afd8227db64e5e0580b08810ab
wgbg <kamil.szela.dev@gmail.com>
2021-02-04 21:45:32 +0100

    update schemas, add get-tree

---
+++ auto-types/api/get-tree.ts
@@ -0,0 +1,55 @@
+/* tslint:disable */
+import { Request, Response } from 'express'
+
+export namespace GetTree{
+  export interface PathRequestParams {
+    entityId: string;
+  }
+  export interface QueryRequestParams {}
+  export interface CookieRequestParams {}
+  export interface HeaderRequestParams {}
+  export type ApiRequest = Request<Required<PathRequestParams>, ResponseBody, {}, QueryRequestParams>
+  export interface ApplicationJson200ResponseBody {
+    data: {
+      /**
+       * Unique id
+       */
+      id: string;
+      /**
+       * Title of root leaf
+       */
+      title: string;
+      createdAt: string;
+    };
+  }
+  export interface ApplicationJson404ResponseBody {
+    meta?: {
+      /**
+       * Meta information about related exception
+       */
+      code: string;
+      debug?: {
+        /**
+         * related debug context object
+         */
+        debug?: {
+          [k: string]: unknown;
+        };
+        /**
+         * related error string representation
+         */
+        err?: string;
+        /**
+         * Error object stringify
+         */
+        errObj?: string;
+        [k: string]: unknown;
+      };
+    } & {
+      code?: "resource-not-found";
+      [k: string]: unknown;
+    };
+  }
+  export type ResponseBody = ApplicationJson200ResponseBody | ApplicationJson404ResponseBody
+  export type ApiResponse = Response<ResponseBody>
+}
