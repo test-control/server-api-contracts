7c425f9203c1e7451b9cc4a8b0688f6d042fb2f2
wgbg <kamil.szela.dev@gmail.com>
2021-02-06 14:21:43 +0100

    add list-tree-test-cases

---
+++ auto-types/api/list-tree-test-cases.ts
@@ -0,0 +1,55 @@
+/* tslint:disable */
+import { Request, Response } from 'express'
+
+export namespace ListTreeTestCases{
+  export interface PathRequestParams {
+    entityId: string;
+  }
+  export interface QueryRequestParams {
+    page?: number;
+    perPage?: number;
+  }
+  export interface CookieRequestParams {}
+  export interface HeaderRequestParams {}
+  export type ApiRequest = Request<Required<PathRequestParams>, ResponseBody, {}, QueryRequestParams>
+  export interface ApplicationJson200ResponseBody {
+    data: {
+      /**
+       * Unique id
+       */
+      id: string;
+      /**
+       * Title
+       */
+      title: string;
+      /**
+       * Test case description
+       */
+      description?: string;
+      /**
+       * Tree id
+       */
+      treeId: string;
+    }[];
+    meta: {
+      /**
+       * Total amount of items
+       */
+      total: number;
+      /**
+       * Items amount per page
+       */
+      perPage: number;
+      /**
+       * Last page number
+       */
+      lastPage: number;
+      /**
+       * Current page number projects
+       */
+      currentPage: number;
+    };
+  }
+  export type ResponseBody = ApplicationJson200ResponseBody
+  export type ApiResponse = Response<ResponseBody>
+}
